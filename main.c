f17a77a7da9a150f3a0564da6c6350132973aa60
knx.bruno <knx.bruno@ab481261-ec2f-0410-a7dc-93bad6e629e8>
2007-05-25 00:43:01 +0000

    Added alter Added help Started parameters

--- main.c
+++ main.c
@@ -3,9 +3,9 @@
 #include <string.h>
 #include <ctype.h>
 
-#include "linked.h"
-#include "utils.h"
 #include "pessoa.h"
+#include "utils.h"
+#include "linked.h"
 #include "manager.h"
 
 #ifndef FILENAME
@@ -13,13 +13,33 @@
 #endif
 
 int main(int argc, char **argv) {
-    struct Pessoa person;
+    struct Pessoa pessoa;
     list lList = NULL;
 
     lList = createList(lList);
 
+    if (argc > 1) {
+        if (!strcmpi("-help", argv[1]) || !strcmpi("-?", argv[1])) {
+            help();
+        }
+        if (!strcmpi("-inserir", argv[1])) {
+            insert(pessoa);
+        }
+        if (!strcmpi("-pesquisar", argv[1])) {
+            search(&lList);
+        }
+        if (!strcmpi("-alterar", argv[1])) {
+            alter(&lList);
+        }
+        if (!strcmpi("-deletar", argv[1])) {
+            deleteReg(&lList);
+        }
+        clearList(lList);
+        exit(0);
+    }
+
     while (1)
-        menu(&person, &lList);
+        menu(&pessoa, &lList);
 
     lList = clearList(lList);
 	return 0;
--- manager.h
+++ manager.h
@@ -2,28 +2,33 @@
 #define MANAGER_H
 
 // menu
-void menu(struct Pessoa *pessoa, list *lList);          // display menu
+void help();                                             // display help stuff
+void menu(struct Pessoa *pessoa, list *lList);           // display menu
 // menu
 
 // Inclusion
-int checkName(struct Pessoa pessoa);                    // check if name is empty
-void resetPessoa(struct Pessoa *pessoa);                // set all elements to "-"
-void writePerson(struct Pessoa pessoa);                 // write the file
-int askInclusion();                                     // check if continue
-struct Pessoa readData(struct Pessoa pessoa);           // read person data
-void insert(struct Pessoa pessoa);                      // begin insertion
+int checkName(struct Pessoa pessoa);                     // set all elements to "-"
+void writePerson(struct Pessoa pessoa);                  // write the file
+int askInclusion();                                      // check if continue
+struct Pessoa readData(struct Pessoa pessoa);            // read person data
+void insert(struct Pessoa pessoa);                       // begin insertion
 // Inclusion
 
 // Search
-int listElement(char element[], list *lList);           // list %element%
-int getElement(char element[]);                         // return element line
-int askSearch();                                        // check if continue
-void search(list *lList);                               // do a search
+int listElement(char element[], list *lList);            // list %element%
+int getElement(char element[]);                          // return element line
+int askSearch();                                         // check if continue
+void search(list *lList);                                // do a search
 // Search
 
+// Alter
+void alterElement(char element[]);                       // deleta e insere
+void alter(list *lList);                                 // chama menu de alteracao
+// Alter
+
 // Delete
-void deleteElement(char element[]);                     // delete a element in the file
-void deleteReg(list *lList);                            // search and destroy
+void deleteElement(char element[]);                      // delete a element in the file
+void deleteReg(list *lList);                             // search and destroy
 // Delete
 
 #endif // MANAGER_H
